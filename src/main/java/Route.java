7251d9cb71646ad8f1d09c319ab67d84ccaefb28
753842_Nanda <nanda123kishore@gmail.com>
2022-04-25 06:32:25 +0000

    Saving changes from StackRoute workbench on Mon Apr 25 2022 12:02:25 GMT+0530 (India Standard Time)

--- src/main/java/Route.java
+++ src/main/java/Route.java
@@ -1,10 +1,69 @@
-class Route{
-String From;
-String To;
-int kms;
-String time;
-String price;
+class Routes{
 
- Route
+	Private String fromCity;
+	Private String toCity;
+	Private String distance;
+	Private String time;
+	Private String price;
 
+	 public routs(String fromCity, String toCity, String distance, String time, String price) {
+        this.fromCity = fromCity;
+        this.toCity = toCity;
+        this.distance = distance;
+        this.time = time;
+        this.price = price;
+
+	  public String getFromCity() {
+        return fromCity;
+    }
+
+    public String getToCity() {
+        return toCity;
+    }
+
+    public String getDistance() {
+        return distance;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+	 public void setFromCity(String fromCity) {
+        this.fromCity = fromCity;
+    }
+
+    public void setToCity(String toCity) {
+        this.toCity = toCity;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+	@Override
+    public String toString() {
+        return "Pavan{" +
+                "fromCity='" + fromCity + '\'' +
+                ", toCity='" + toCity + '\'' +
+                ", distance='" + distance + '\'' +
+                ", time='" + time + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+
+    }
+	
 }
